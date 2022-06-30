4c4f2ff8d3d89caa7bf9b840150554895ae4dd3d
aidy 123 <108388740+AWSM1@users.noreply.github.com>
2022-06-30 19:07:15 +0100

    Add files via upload

---
+++ Dingusdongus2.0.py
@@ -0,0 +1,4 @@
+print('My second python script!! L:)')
+print('Thanks to my friend Daniel who taught me everything I know so far!')
+print('Thanks Kanye Yeast... massive shoutout bro!!!!!!!!!!!!!')
+
